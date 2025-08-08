a705badcd6e0e64c57472f15252311a70f69ffc8
suima0713 <savannanohara@gmail.com>
2025-08-08 09:12:12 +0900

    test: add dummy test files for CI validation

---
+++ tests/test_dummy.py
@@ -0,0 +1,9 @@
+"""Dummy test file for CI"""
+
+def test_dummy():
+    """Basic test to ensure pytest runs"""
+    assert True
+
+def test_simple():
+    """Another simple test"""
+    assert 1 + 1 == 2
